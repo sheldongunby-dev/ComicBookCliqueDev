f296deb9937b1baf7689e6ca2d3ba28fb4af4bf1
Sheldon Gunby <sheldongunby-dev@users.noreply.github.com>
2026-06-06 20:17:59 -0400

    chore: point scraper BASE_URL to built-in Squarespace domain

--- scripts/scrape-all-content.js
+++ scripts/scrape-all-content.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const BASE_URL = 'https://comicbookclique.com';
+const BASE_URL = 'https://comicbookclique.squarespace.com';
 const TARGET_DIR = path.join(__dirname, '../src/content/generated');
 
 // Map endpoints to our local JSON schema generators
