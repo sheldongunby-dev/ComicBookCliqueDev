13043be04fb827b9aa07a08a1ced9ed1f7c127db
Sheldon Gunby <sheldongunby-dev@users.noreply.github.com>
2026-06-10 02:13:38 -0400

    fix: typo in sanity project ID

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-06-07'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'huoyli9r'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'huoy119r'
 export const useCdn = false
